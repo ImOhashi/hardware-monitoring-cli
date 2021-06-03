938bd4d842c0b5853e92c9aa32edec9c730d56f3
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-06-03 01:13:43 -0300

    document

--- src/utils/chalk.ts
+++ src/utils/chalk.ts
@@ -1,5 +1,12 @@
 import chalk from "chalk";
 
+/**
+ * Return a painted text
+ *
+ * @param {string} text
+ * @param {string} color
+ * @returns {string}
+ */
 export function paintText(text: string, color: string): string {
   return chalk[color](text);
 }

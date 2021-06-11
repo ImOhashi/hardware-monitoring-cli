0ec492d899f8bed56fdad848a102e963f81c166e
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-06-11 16:42:57 -0300

    remove redis

--- src/tools/systemUsers.ts
+++ src/tools/systemUsers.ts
@@ -1,41 +1,24 @@
 import si from "systeminformation";
 import CliTable from "cli-table";
 
-import { Redis } from "../cache";
-
 export class OperatingSystem {
   public async getAllSystemUsers(): Promise<void> {
-    try {
-      const redis = new Redis();
+    const users = await si.users();
 
-      const cachedUsers = await redis.get("local-users");
+    const usersTable = new CliTable({
+      head: ["user", "tty", "time", "ip", "date"],
+    });
 
-      if (cachedUsers) {
-        console.log(cachedUsers);
-        return;
-      } else {
-        const users = await si.users();
+    users.forEach((value) => {
+      usersTable.push([
+        value.user,
+        value.tty,
+        value.time,
+        value.ip,
+        value.date,
+      ]);
+    });
 
-        const usersTable = new CliTable({
-          head: ["user", "tty", "time", "ip", "date"],
-        });
-
-        users.forEach((value) => {
-          usersTable.push([
-            value.user,
-            value.tty,
-            value.time,
-            value.ip,
-            value.date,
-          ]);
-        });
-
-        redis.set("local-users", usersTable, 15 * 60);
-
-        console.log(usersTable.toString());
-      }
-    } catch (err) {
-      throw new Error(err);
-    }
+    console.log(usersTable.toString());
   }
 }

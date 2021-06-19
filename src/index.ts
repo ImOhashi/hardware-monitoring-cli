f850309c817818a32809c22f6c38cbac29b01993
Majulia <majulia_carvalho@yahoo.com.br>
2021-06-18 22:03:22 -0300

    Adding a connection database with mongoose :)

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import commander from "commander";
+import { config } from "dotenv";
 
+import database from "./database/connection";
 import { writeTitle } from "./utils";
 import { OperatingSystem } from "./tools";
 
 const os = new OperatingSystem();
+
+config();
+
+database.connection();
 
 writeTitle(process.env.npm_package_name, "red");
 

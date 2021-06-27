7e57a3299399da81d4e7dc8b77a1215573e40cd2
ImOhashi <leonardoohashi.mkt@gmail.com>
2021-06-27 16:59:57 -0300

    feat: add a amqp publish method and connection

---
+++ src/broker/rabbitmq.ts
@@ -0,0 +1,27 @@
+import amqp from "amqplib";
+
+export class RabbitmqServer {
+  private channel: amqp.Channel;
+
+  public async connect(): Promise<void> {
+    try {
+      const conn = await amqp.connect(
+        `amqp://${process.env.AMQP_USER}:${process.env.AMQP_PASS}@${process.env.AMQP_HOST}:${process.env.AMQP_PORT}`
+      );
+      console.log("Queue connected!");
+
+      this.channel = await conn.createChannel();
+      console.log("Queue created!");
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  public async publishInExchange(
+    exchange: string,
+    routingKey: string,
+    message: string
+  ): Promise<boolean> {
+    return this.channel.publish(exchange, routingKey, Buffer.from(message));
+  }
+}
--- src/error/database-error.ts
+++
@@ -1,8 +0,0 @@
-import { NativeError } from "mongoose";
-
-export class DatabaseError extends Error {
-  constructor(paramName?: NativeError) {
-    super(`Database isn't connected \n${paramName}`);
-    this.name = "DatabaseError";
-  }
-}
